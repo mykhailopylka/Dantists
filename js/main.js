bab15623e76171a7b6c88d9942853c74e04ac51e
origin <mykhailo.pylka@gmail.com>
2020-09-21 15:42:13 +0300

    project dantists

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// slick slider
+$(document).ready(function () {
+  $(".welcome-descriptiopn__slider").slick({
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/left-arrow-b.svg" alt="arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/right-arrow-b.svg" alt="arrow"></button>',
+  });
+});
+// testimonials------------------------
+$(document).ready(function () {
+  $(".testimonials-slider").slick({
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/left-arrow-w.svg" alt="arrow"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/right-arrow-w.svg" alt="arrow"></button>',
+  });
+});
+// трансформація бутербродного меню в хрестик і назад
+$(function () {
+  document.querySelector(".menu-icon-wrapper").onclick = function () {
+    document.querySelector(".menu-icon").classList.toggle("menu-icon-active");
+  };
+});
+// виїзд меню при натисканні на іконку бутербодного меню
+$(function () {
+  $(".menu-icon-wrapper").on("click", function () {
+    $(".menu-list").slideToggle(300, function () {
+      if ($(this).css("display") === "none") {
+        $(this).removeAttr("style");
+      }
+    });
+  });
+});
+
+// анімація lotti
+var elem = document.getElementById("main-invitation__svgContainer");
+var animData = {
+  container: elem,
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "../js/TextAnimation_1.json",
+};
+anim = lottie.loadAnimation(animData);
